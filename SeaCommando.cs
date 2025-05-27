f684fb5a953021fe63e8cb7b09f5ab0cce499d9c
orel <0533orel@gmail.com>
2025-05-27 12:03:11 +0300

    create class SeaCommando

---
+++ SeaCommando.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandosOop.Models
+{
+    public class SeaCommando : Commando
+    {
+        public SeaCommando(string name, string codeName) : base(name, codeName) { }
+
+        public void CanSwim()
+        {
+            Console.WriteLine("The soldier can swim");
+        }
+    }
+}
